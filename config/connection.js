8d6c2798d2c111d1839d58a7d094b90b478382c7
Thomas Dunlap <ttdunlap33@gmail.com>
2021-05-15 18:07:38 -0600

    heroku credentials connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,12 +3,23 @@ require('dotenv').config();
 const Sequelize = require('sequelize');
 const sequelize = process.env.JAWSDB_URL
 
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+  connection= mysql.createConnection({
     host: 'localhost',
-    dialect: 'mysql',
-    dialectOptions: {
-      decimalNumbers: true,
-    },
+    user: 'root',
+    password: 'root',
+    database: 'ecommerce_db'
   });
+};
+
+  // ? new Sequelize(process.env.JAWSDB_URL)
+  // : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  //   host: 'localhost',
+  //   dialect: 'mysql',
+  //   dialectOptions: {
+  //     decimalNumbers: true,
+  //   },
+  // });
 module.exports = sequelize;
